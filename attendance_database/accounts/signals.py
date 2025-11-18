03f062e6f11737b4d91310157671e927aef9cbbf
ZeroChad <drakeescarda13@gmail.com>
2025-11-18 20:55:14 +0800

    little fix

--- attendance_database/accounts/signals.py
+++ attendance_database/accounts/signals.py
@@ -8,46 +8,45 @@ from .models import CustomUser
 @receiver(post_migrate)
 def create_admin_user(sender, **kwargs):
     if sender.name == "accounts":
-        if sender.name == "accounts":
-            users = [{
-                "email": get_secret("ADMIN_EMAIL"),
-                "role": "admin",
-                "admin": True,
-            },{
-                "email": "employee@test.com",
-                "role": "employee",
-                "admin": False,
-            }, {
-                "email": "manager@test.com",
-                "role": "manager",
-                "admin": False,
-            }]
+        users = [{
+            "email": get_secret("ADMIN_EMAIL"),
+            "role": "admin",
+            "admin": True,
+        },{
+            "email": "employee@test.com",
+            "role": "employee",
+            "admin": False,
+        }, {
+            "email": "manager@test.com",
+            "role": "manager",
+            "admin": False,
+        }]
 
-            for user in users:
-                USER = CustomUser.objects.filter(email=user["email"]).first()
-                if not USER:
-                    if user["admin"]:
-                        USER = CustomUser.objects.create_superuser(
-                            email=user["email"],
-                            username=user["email"],
-                            password=get_secret("ADMIN_PASSWORD"),
-                            sex="Male",
-                            birthday=localdate(),
-                            role=user["role"],
-                        )
-                    else:
-                        USER = CustomUser.objects.create_user(
-                            email=user["email"],
-                            username=user["email"],
-                            password=get_secret("ADMIN_PASSWORD"),
-                            sex="Male",
-                            birthday=localdate(),
-                            role=user["role"]
-                        )
-                    print(f"Created {user['role']} account: {USER.email}")
+        for user in users:
+            USER = CustomUser.objects.filter(email=user["email"]).first()
+            if not USER:
+                if user["admin"]:
+                    USER = CustomUser.objects.create_superuser(
+                        email=user["email"],
+                        username=user["email"],
+                        password=get_secret("ADMIN_PASSWORD"),
+                        sex="Male",
+                        birthday=localdate(),
+                        role=user["role"],
+                    )
+                else:
+                    USER = CustomUser.objects.create_user(
+                        email=user["email"],
+                        username=user["email"],
+                        password=get_secret("ADMIN_PASSWORD"),
+                        sex="Male",
+                        birthday=localdate(),
+                        role=user["role"]
+                    )
+                print(f"Created {user['role']} account: {USER.email}")
 
-                    USER.first_name = f"DEBUG_USER:{USER.email}"
-                    USER.is_active = True
-                    USER.save()
+                USER.first_name = f"DEBUG_USER:{USER.email}"
+                USER.is_active = True
+                USER.save()
 
         
