fd129fa34c2188dae25b3bb9a50d4cb03d592fc9
ZeroChad <drakeescarda13@gmail.com>
2025-11-18 15:51:07 +0800

    CustomUser Models Account

--- attendance_database/accounts/models.py
+++ attendance_database/accounts/models.py
@@ -1,3 +1,46 @@
 from django.db import models
+from django.contrib.auth.models import AbtractUser
+from django.utils.timezone import now, localdate
+from datetime import datetime, date, time , timedelta
 
-# Create your models here.
+class CustomUser(AbtractUser):
+    email = models.EmailField(unique=True)
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = []
+
+    ROLE_CHOICES = (
+        ('admin', 'Admin'),
+        ('employee', 'Employee'),
+        ('manager', 'Manager'),
+    )
+
+    role = models.CharField(max_length=20, choices=ROLE_CHOICES, default='employee')
+
+    SEX_CHOICES = (
+        ("male", "Male"),
+        ("female", "Female"),
+        ("other", "Other"),
+    )
+    sex = models.CharField(max_length=10, choices=SEX_CHOICES, default='male')
+    birthday = models.DateField(default=now)
+    date_joined = models.DateField(default=now, editable=False)
+
+    @property
+    def age(self):
+        date_now = localdate(now())
+        age = (
+            date_now.year
+            - self.birthday.year
+            - ((date_now.month, date_now.day) < (self.birthday.month, self.birthday.day))
+        )
+        return age
+
+    @property
+    def full_name(self):
+        return f"{self.first_name} {self.last_name}"
+    
+    def save(self, *args, **kwargs):
+        self.email = self.email.lower()
+        super().save(*args, **kwargs)
+
+
